7f3349d06c78821f37462fe9d15e470e21cd487a
nefir2 <vadim.ahtamov@gmail.com>
2022-06-11 16:51:28 +0300

    (decimal)null fix

--- TRIPPLESIX/celc.cs
+++ TRIPPLESIX/celc.cs
@@ -312,7 +312,7 @@ namespace TRIPPLESIX
 				problemBox.Text = $"{ans}";
 
 				//установка первого числа для бесконечного действия.
-				prevNum = (decimal)right;
+				prevNum = right ?? 0;
 
 				//восстановление значений полей.
 				left = null;
